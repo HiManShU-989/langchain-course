18bdab3d5ed96126a150734f2c4d0562ba62438c
Himanshu Singh <himanshu.singh13@globallogic.com>
2026-05-02 15:51:05 +0530

    Created basic search agent with default message return.

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain.agents import create_agent #Modern way to create agents. This function abstracts away the details of how to set up an agent, and allows you to create agents with just a few lines of code.
+from langchain.tools import tool #Under the hood, tools are callable functions with well-defined inputs and outputs that get passed to a chat model. The model decides when to invoke a tool based on the conversation context, and what input arguments to provide.
+from langchain_core.messages import HumanMessage
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+@tool     #Makes a tool with the description, the arguments which LLM can use to decide if it can call the tool or not.
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query (str): The search query
+    Returns:
+        str: The search results
+    """
+    print(f"Seraching for {query}...")
+    return "Tokyo's weather is sunny."
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash") #Initialize the LLM
+tools = [search] #List of tools to be used by the agent
+agent = create_agent(model = llm, tools = tools) #Create an agent with the LLM and the tools
+
+
 def main():
-    print("Hello from langchain-course!")
+    result = agent.invoke({"messages": HumanMessage(content="What is the weather in Tokyo?")}) #Invoke the agent with a human message. The agent will decide whether to call the tool or not based on the message and the tool's description.
+    print(result)
+    # print("Hello from langchain-course!")
 
 
 if __name__ == "__main__":
