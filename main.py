4c9939dcf54022eea9e00dc9799c5d3edc355c51
Himanshu Singh <himanshu.singh13@globallogic.com>
2026-07-01 11:49:07 +0530

    Raw function on tool calling implementation

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
 from dotenv import load_dotenv
-load_dotenv()
-from langchain.chat_models import init_chat_model
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage, SystemMessage, ToolMessage
+load_dotenv() 
+import ollama
 from langsmith import traceable
 
 MAX_ITERATIONS = 10
 MODEL = "qwen3.5:latest"
 
-# Tools ----- Decorators for making tools(langchain) ------
-@tool
+# Langcahin tool decorators allow us to easily create tools that can be called by the agent.
+# But using raw functions we have to write provider specific code to make them work with the agent.
+# The traceable decorator from langsmith allows us to easily create tools that can be called by the agent 
+# without having to write provider specific code. It also allows us to track the execution of the tools in the langsmith dashboard.
+
+tools_for_llm = [
+    {
+        "type": "function",
+        "function":{
+            "name": "get_product_price",
+            "description": "Look up the price of a product in the catalog.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "product": {
+                        "type": "string",
+                        "description": "The name of the product to look up e.g. 'laptop', 'headphones', 'keyboard'."
+                    }
+                },
+                "required": ["product"]
+            }
+        }
+    },
+    {
+       "type": "function",
+       "function": {
+           "name": "apply_discount",
+           "description": "Apply a discount to a price based on the discount tier and return the final price.",
+           "parameters": {
+               "type": "object",
+               "properties": {
+                   "price": {
+                       "type": "number",
+                       "description": "The original price of the product."
+                   },
+                   "discount_tier": {
+                       "type": "string",
+                       "description": "The discount tier to apply. Available tiers are 'bronze', 'silver', and 'gold'."
+                   }
+               },
+               "required": ["price", "discount_tier"]
+           }
+       } 
+    }
+] 
+@traceable(run_type = "tool")
 def get_product_price(product:str) -> float:
     """Look up the price of a product in the catalog.
     
@@ -26,7 +68,7 @@ def get_product_price(product:str) -> float:
     }
     return prices.get(product, 0.0)
 
-@tool
+@traceable(run_type = "tool")
 def apply_discount(price:float, discount_tier:str) -> float:
     """Apply a discount to a price based on the discount tier and return the final price.
     
@@ -46,16 +88,26 @@ def apply_discount(price:float, discount_tier:str) -> float:
     return round(price * (1 - discount_rate / 100), 2)
 
 
+# Without langchain, we have to manually trace the LLM calls.
+@traceable(name="Ollama Chat", run_type = "llm")
+def ollama_chat_traced(messages):
+    return ollama.chat(model = MODEL, messages = messages, tools = tools_for_llm)
+
+
+
 # _______AGENT LOOP _________
-@traceable(name = "Langchain agent loop")
+@traceable(name = "Ollama agent loop")
 def run_agent(question: str):
-    tools = [get_product_price, apply_discount] #List of available tools
-    tools_dict = {t.name : t for t in tools}
-    llm = init_chat_model(f"ollama:{MODEL}", temperature = 0.0)
-    llm_with_tools = llm.bind_tools(tools)
+    tools_dict = {
+        "get_product_price": get_product_price,
+        "apply_discount": apply_discount
+    }
+    
+    # On using langchain, we don't have to worry about the prompt formatting or how to pass tool results back to the LLM.
     messages = [
-        SystemMessage(content = 
-                      "You are a helpful shopping assistant."
+        {
+            "role": "system",
+            "content": "You are a helpful shopping assistant."
                       "You have access to a product catalog tool"
                       "and a discount tool.\n\n"
                       "STRICT RULES- you must follow these exactly:\n"
@@ -67,13 +119,19 @@ def run_agent(question: str):
                       "3. NEVER calculate discount yourself using math."
                       "Always use the apply_discount tool.\n"
                       "4. If the user does not specify a discount tier,"
-                      "ask them which tier to use - do NOT assume one."),
-        HumanMessage(content = question) 
+                      "ask them which tier to use - do NOT assume one."
+        },
+        {
+            "role":"user",
+            "content":question
+        }
     ]
     
     for iteration in range(1,MAX_ITERATIONS + 1):
         print(f"--- Iteration {iteration} ---")
-        ai_message = llm_with_tools.invoke(messages)
+        # Ollama.chat() directly instead of llm_with_tools.invoke()
+        response = ollama_chat_traced(messages=messages)
+        ai_message = response.message
         tool_calls = ai_message.tool_calls
         # if no tool calls means we have the final answer, break the loop and return the answer
         if not tool_calls:
@@ -82,9 +140,9 @@ def run_agent(question: str):
         
         #Process only first tool call- force one tool per iteration for simplicity
         tool_call = tool_calls[0]
-        tool_name = tool_call.get("name")
-        tool_args = tool_call.get("args", {})
-        tool_call_id = tool_call.get("id")
+        # Difference in attribute access method.
+        tool_name = tool_call.function.name
+        tool_args = tool_call.function.arguments
         
         print(f" [Tool Selected]: {tool_name} with args: {tool_args} ")
         
@@ -92,11 +150,15 @@ def run_agent(question: str):
         if tool_to_use is None:
             raise ValueError(f"Tool {tool_name} not found in available tools.")
         
-        observation = tool_to_use.invoke(tool_args)
+        # Direct function call instead of tool.invoke()
+        observation = tool_to_use(**tool_args)
         print(f" [Tool Result]: {observation} \n")
         
         messages.append(ai_message)
-        messages.append(ToolMessage(content = str(observation), tool_call_id = tool_call_id))
+        messages.append({
+            "role":"tool",
+            "content":str(observation)
+        })
             
     print("ERROR: Max iterations reached without a final answer.")
     return None
